11574cf0866b1379d08b2f6ae8150ab448c08669
Ivan Montiel <idmontie@asu.edu>
2015-10-07 09:37:31 -0700

    Add slick images and custom css

--- functions.php
+++ functions.php
@@ -1,11 +1,13 @@
 <?php
 function slick_scripts () {
-  wp_register_style( 'slick', get_stylesheet_directory_uri().'/stylesheets/slick.css', array(), '0.0.2', 'all' );
+  wp_register_style( 'slick', get_stylesheet_directory_uri().'/stylesheets/slick.css', array(), '0.0.1', 'all' );
+  wp_register_style( 'slick-custom', get_stylesheet_directory_uri().'/stylesheets/slick-custom.css', array(), '0.0.1', 'all' );
 
-  wp_enqueue_style( 'sustainability-solutions-wordpress-theme' );
+  wp_enqueue_style( 'slick' );
+  wp_enqueue_style( 'slick-custom' );
 
-  wp_register_script( 'slick-wp-script', get_stylesheet_directory_uri() . '/javascripts/slick-starter.js', array(), '0.0.1', 'all' );
   wp_register_script( 'slick', get_stylesheet_directory_uri().'/javascripts/slick.min.js', array(), '0.0.1', 'all' );
+  wp_register_script( 'slick-wp-script', get_stylesheet_directory_uri() . '/javascripts/slick-starter.js', array(), '0.0.1', 'all' );
 
   wp_enqueue_script( 'slick' );
   wp_enqueue_script( 'slick-wp-script' );
